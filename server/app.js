193e39ef094c8bd5966d09509ea032d65d6a2ceb
Gavin Wilson <Gavin Wilson>
2017-01-30 15:30:13 -0500

    sequelize

--- server/app.js
+++ server/app.js
@@ -1,9 +1,67 @@
 var express = require('express');
 var app = express();
-app.listen(3000, function(){
-	console.log("app is listening on 3000");
+var bodyParser = require('body-parser');
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('workoutlog', 'postgres', 'Zelda123', {
+	host: 'localhost',
+	dialect: 'postgres'
 });
+
+sequelize.authenticate().then(
+	function() {
+		console.log('connected to workoutlog postgres db');
+	},
+	function(err){
+		console.log(err);
+	}
+);
+
+// build a user model in sqllize
+var User = sequelize.define('user', {
+	username: Sequelize.STRING,
+	passwordhash: Sequelize.STRING,
+});
+
+//creates the table in postgres
+//matches the model we defined
+//Doesn't drop the db
+User.sync(); 
+// User({ force: true }); //drops the table compeletly (line 27ish)
+
+app.use(bodyParser.json());
+
+app.post('/api/user', function(req, res){
+	//when we post to api user, it will want a user object in the body
+	var username = req.body.user.username;
+	var pass = req.body.user.password;	//TODO: hash this password - HASH=not human readable
+
+	//Match the model we create above
+	//Sequelize - take the user model and go out to the db and create this:
+	User.create({
+		username: username,
+		passwordhash: ''
+}).then(
+		//Sequelize is going to return the object it created from db.
+		function createSuccess(user){
+			//successful get thsi:
+			res.json({
+				user: user,
+				message: 'create'
+			});
+		},
+		function createError(err){
+			res.send(500, err.message);
+		}
+	);
+});
+
+app.use(require('./middleware/headers'));
+
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
-app.use(require('./middleware/headers'));
+});
+
+app.listen(3000, function(){
+	console.log("app is listening on port 3000");
 });
