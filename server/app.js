d083e50be5e58117d7d4c0376b8033ded77ee73a
Gavin Wilson <Gavin Wilson>
2017-01-30 20:01:35 -0500

    routes

--- server/app.js
+++ server/app.js
@@ -19,30 +19,7 @@ User.sync();
 
 app.use(bodyParser.json());
 
-app.post('/api/user', function(req, res){
-	//when we post to api user, it will want a user object in the body
-	var username = req.body.user.username;
-	var pass = req.body.user.password;	//TODO: hash this password - HASH=not human readable
 
-	//Match the model we create above
-	//Sequelize - take the user model and go out to the db and create this:
-	User.create({
-		username: username,
-		passwordhash: ''
-}).then(
-		//Sequelize is going to return the object it created from db.
-		function createSuccess(user){
-			//successful get thsi:
-			res.json({
-				user: user,
-				message: 'create'
-			});
-		},
-		function createError(err){
-			res.send(500, err.message);
-		}
-	);
-});
 
 app.use(require('./middleware/headers'));
 
